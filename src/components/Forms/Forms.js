47e538e7ccd50e26826a8400ce3832170c33f655
gesali@bitbucket.org <mali@globalbposervices.com>
2019-10-01 17:53:38 +0500

    seconf commit

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+
+
+
 
 class Forms extends Component {
 
     render() {
+     const SignupSchema = Yup.object().shape({
+        firstName: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+        lastName: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+        email: Yup.string()
+          .email('Invalid email')
+          .required('Required'),
+      });
     return (
   <div>
-  <h1>Anywhere in your app!</h1>
+  <h1>Formik in your app Without YUP!</h1>
   <Formik
     initialValues={{ email: '', password: '' }}
-    validate={values => {
+    validate = {values => {
+      debugger
       let errors = {};
       if (!values.email) {
         errors.email = 'Required';
@@ -23,7 +42,9 @@ class Forms extends Component {
       }
       return errors;
     }}
+    debugger
     onSubmit={(values, { setSubmitting }) => {
+      debugger
       setTimeout(() => {
         alert(JSON.stringify(values, null, 2));
         setSubmitting(false);
@@ -59,6 +80,38 @@ class Forms extends Component {
       </form>
     )}
   </Formik>
+
+  <h1>Formik in your app With YUP!</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+
+
 </div>
     );
   }
